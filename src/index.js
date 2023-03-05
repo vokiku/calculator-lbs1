4974208e8938a596a3c254b827e6e489bb3df62b
Vokiku <vokiku@gmail.com>
2023-03-05 12:38:59 +0500

    instal eslint and config webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import './assets/styles/style.scss'
+
 console.log('Компиляция из файла в файл')
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,65 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 function fileName (ext) {
-    return `[name].bundle.${ext}`
+  return `[name].bundle.${ext}`
 }
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        main: [
-            'core-js/stable',
-            'regenerator-runtime/runtime',
-            '/index.js'
-        ]
-    },
-    output: {
-        filename: fileName('js'),
-        path: path.resolve(__dirname, 'dist'), 
-        clean: true
-    },
-    devServer: {
-        port: 3000,
-        open: true,
-        hot: true,
-        watchFiles: './'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-        template: "./index.html"
-}),
-        new CopyPlugin({
-        patterns: [
-            { from: path.resolve(__dirname, 'src', 'img', 'favicon.ico'), to: path.resolve(__dirname, 'dist') },
-        ],
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: [
+      'core-js/stable',
+      'regenerator-runtime/runtime',
+      './index.js'
+    ]
+  },
+  output: {
+    filename: fileName('js'),
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+    watchFiles: './'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
     }),
-        new MiniCssExtractPlugin({
-            filename: fileName('css')
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                     "css-loader",
-                     "sass-loader"
-                    ],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: "babel-loader",
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
-                }
-              }
-        ],
-    }
- }
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'src', 'img', 'favicon.ico'), to: path.resolve(__dirname, 'dist') }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: fileName('css')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
